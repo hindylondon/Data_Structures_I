8ec4301db19c955b9d1367b59903a328f53a1469
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-11-05 21:05:43 -0500

    Update ReverseString.java

--- DataStructuresI/src/week5/ReverseString.java
+++ DataStructuresI/src/week5/ReverseString.java
@@ -3,11 +3,22 @@ package week5;
 public class ReverseString {
 	public static void main(String[] args) {
 		
-		char letters[] = "hello".toCharArray();
-
-		for( int i = letters.length - 1; i >= 0; i--) {
-			System.out.print(letters[i]);
-		}
+		//char letters[] = "hello".toCharArray();
+		
+		String word = "hello";
+		
+		System.out.print(reverse(word));
+		
 	}
+		
+		public static String reverse(String word) {
+			if (word.length() == 0) {
+				return word;
+			}
+			else {
+				System.out.print(word.charAt(word.length()-1));
+				return reverse(word.substring(0, word.length() - 1));
+			}
+		}
 
 }
