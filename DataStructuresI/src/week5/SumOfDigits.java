c147c85df7eb18b6826b38c32cf89cca5a48a246
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-11-05 21:06:04 -0500

    Update SumOfDigits.java

--- DataStructuresI/src/week5/SumOfDigits.java
+++ DataStructuresI/src/week5/SumOfDigits.java
@@ -3,18 +3,19 @@ package week5;
 public class SumOfDigits {
 	public static void main(String[] args) {
 		int num = 12345;
-		int result = sum(num);
-		System.out.println(result);
+		System.out.println(sum(num));
 		
 	
 	}
 	public static int sum(int num) {
 		int sum = 0;
-		while (num > 0) {
-			sum = sum + num % 10;
-			num = num / 10;
+		if (num == 0) {
+			return sum;
 		}
-		return sum;
+		else {	
+			sum = num % 10;
+			return sum + sum(num / 10);
+		}
 	}
 
 }
