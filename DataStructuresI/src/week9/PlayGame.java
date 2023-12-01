a21974b2e6fc05a47d9ec8a2f4e24de187b02fb6
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-11-30 23:15:56 -0500

    Update PlayGame.java

--- DataStructuresI/src/week9/PlayGame.java
+++ DataStructuresI/src/week9/PlayGame.java
@@ -1,92 +1,85 @@
 package week9;
 
-import java.util.*;
-
 public class PlayGame {
-	//fields
+	// fields
 	private Queue<PlayerInterface> players;
-	private Board board;
-	private PlayerInterface currentTurn;
-	private HumanPlayer human;
-	private CompPlayer comp;
+	private static HumanPlayer human;
+	private static CompPlayer comp;
 
-	// set getters and setters
-
-	//constructors
-	public PlayGame(Queue<PlayerInterface> players, Board board, PlayerInterface currentTurn, HumanPlayer human,
+	// constructors
+	public PlayGame(Queue<PlayerInterface> players, HumanPlayer human,
 			CompPlayer comp) {
 		this.players = players;
-		this.board = board;
 		this.human = human;
 		this.comp = comp;
 	}
 
-	//players turn: sets both the human and comp symbol and calls their turn methods in order of the queue
-	//keeps going until there is a winner
-	public void turn() {
+	// players turn: sets both the human and comp symbol and calls their
+	// turn methods in order of the queue
+	// keeps going until there is a winner
+	public int turn() {
 		char humanPiece = human.getSymbol();
 		char compPiece = comp.getSymbol();
 		boolean checkWin = false;
+		int point = 0;
 
-		do {
-			if (currentTurn == human) {
-			players.dequeue();
-			int[] spot = human.go();
-			while (Board.board[spot[0]][spot[1]] != ' ') {
-				System.out.println("This spot is taken. Choose another spot.");
-				spot = human.go();
+		//while there is now winner and there is still space
+		//it will call the human and comp turns again and again to play the game
+		//returns point if human wins
+		while (!checkWin && !Board.isBoardFull()){
+			if (players.peek() == human) {
+				players.dequeue();
+				int[] spot = human.go();
+				while (Board.board[spot[0]][spot[1]] != ' ') {
+					System.out.println("This spot is taken. Choose another spot.");
+					spot = human.go();
+				}
+				Board.board[spot[0]][spot[1]] = humanPiece;
+				Board.DisplayBoard();
+				checkWin = Board.checkWinner(human.getSymbol(), spot[0], spot[1]);
+
+				if (checkWin) {
+					point = endGame(human);
+					break;
+				}
+				players.enqueue(human);
+				
+			} else {
+				players.dequeue();
+				int[] spot = comp.go();
+				System.out.println("Computer goes...");
+				Board.board[spot[0]][spot[1]] = compPiece;
+				Board.DisplayBoard();
+				checkWin = Board.checkWinner(comp.getSymbol(), spot[0], spot[1]);
+
+				if (checkWin) {
+					point = endGame(comp);
+					break;
+				}
+				players.enqueue(comp);
 			}
-			Board.board[spot[0]][spot[1]] = humanPiece;
-			Board.DisplayBoard();
-			checkWin = Board.checkWinner(human.getSymbol(), spot[0], spot[1]);
-
-			if (checkWin) {
-				endGame(human);
-			}
-
-			players.enqueue(human);
-		} else {
-			players.dequeue();
-			int[] spot = comp.go();
-			
-			Board.board[spot[0]][spot[1]] = compPiece;
-			Board.DisplayBoard();
-			checkWin = Board.checkWinner(human.getSymbol(), spot[0], spot[1]);
-
-			if (checkWin) {
-				endGame(human);
-			}
-			players.enqueue(comp);
+				
 		}
+		//board = full and no winner -> tie
+		if (Board.isBoardFull()) {
+			System.out.println("Tie! Play again to win!");
 		}
-		while (checkWin = false && checkEmpty(board));
+		return point;
+	}
+
+
+	// ends the game, displays congrats msg, returns points if human wins
+	public static int endGame(PlayerInterface player) {
+		int point = 0;
+		
+		if (player == human) {
+			System.out.println("Congratulations! You are the winner!");
+			point = 1;
+		}
+		else if(player == comp){
+			System.out.println("Computer wins! Play again to win!");
+		}
+		return point;
 	}
 	
-	//checks if the board still has empty spaces
-	//if not, will return false and the game will be a draw
-	public static boolean checkEmpty(Board board) {
-		boolean hasEmpty = false;
-		
-		for(int i = 0; i < board.getLength(); i++) {
-			for (int j = 0; j < board.getLength(); j++) {
-				if (Board.board[i][j] == ' ') {
-					hasEmpty = true;
-					return hasEmpty;
-				}
-			}
-		}
-		return hasEmpty;
-	}
-
-	//ends the game, displays congrats msg
-	//asks the user if he wants to play again
-	public static char endGame(PlayerInterface player) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Congratulations! " + player + " is the winner!");
-		System.out.println("Do you want to play again? (y/n) ");
-		char playAgain = scanner.next().charAt(0);
-		scanner.close();
-		return playAgain;
-	}
-
 }
