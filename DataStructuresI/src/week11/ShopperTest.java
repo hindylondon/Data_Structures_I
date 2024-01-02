38f3c2807503d8cb8d88711eeb041786d03d8543
hindylondon <119772445+hindylondon@users.noreply.github.com>
2024-01-01 19:58:52 -0500

    Update ShopperTest.java

--- DataStructuresI/src/week11/ShopperTest.java
+++ DataStructuresI/src/week11/ShopperTest.java
@@ -14,42 +14,28 @@ class ShopperTest {
 
 	@Test
 	void testIfStableSort() {
-		Shopper s1 = new Shopper("h", 2, 3);
-		Shopper s2 = new Shopper("i", 4, 6);
-		Shopper s3 = new Shopper("j", 4, 6);
-
 		MyPriorityQueue<Shopper> pq = new MyPriorityQueue<>();
+		
+		// create 2 shopper with equal totalPurchaseValue
+		Shopper second = new Shopper("i", 4, 6);
+		Shopper third = new Shopper("j", 4, 6);
+		Shopper first = new Shopper("h", 4, 8);
+		
+		pq.enqueue(second);
+		pq.enqueue(third);
+		pq.enqueue(first);
+		
+		//sort and check for stable
+		Shopper s1 = pq.dequeue();
+		Shopper s2 = pq.dequeue(); 
+		Shopper s3 = pq.dequeue();
+		
 
-		pq.enqueue(s1);
-		pq.enqueue(s2);
-		pq.enqueue(s3);
+		assertEquals(s1.getName(), "h");
+		assertEquals(s2.getName(), "i");
+		assertEquals(s3.getName(), "j");
+		
 
-		assertTrue(isStableOrder(pq, s1, s2, s3));
-	}
-
-	private boolean isStableOrder(MyPriorityQueue<Shopper> pq, Shopper s1, Shopper s2, Shopper s3) {
-		Shopper high = pq.dequeue();
-		Shopper med = pq.dequeue();
-		Shopper low = pq.dequeue();
-		if (high.getTotalPurchase() < med.getTotalPurchase() || high.getTotalPurchase() < low.getTotalPurchase()) {
-			return false;
-		} else if (med.getTotalPurchase() < low.getTotalPurchase()) {
-			return false;
-		} else if (high.getTotalPurchase() == med.getTotalPurchase()) {
-			if (high.getId() > med.getId()){
-				return false;
-			}
-		}	
-			pq.enqueue(high);
-			pq.enqueue(med);
-			pq.enqueue(low);
-			
-			//check if order is kept
-			Shopper first = pq.dequeue();
-			Shopper second = pq.dequeue();
-			Shopper third = pq.dequeue();
-			
-		return first.equals(s2) && second.equals(s3) && third.equals(s1);
 	}
 
 }
